fced2c23b419bbd51930b3a5347256d887c3f0a5
Tanishka Sharma <anjanasharma8448766174@gmail.com>
2025-07-22 11:43:00 +0530

    done

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -7,6 +7,6 @@ const userSchema=new mongoose.Schema({
 },
 {versionKey:false})
 
-const userModel=mongoose.model("user",userSchema);
+const userModel=mongoose.model("User",userSchema);
 
 module.exports=userModel
